e323bdf359991fe8b7c1adc5ddc9348cd60a7062
bianca482 <biancakasper@gmx.at>
2024-07-03 14:28:29 +0200

    moved teastore and test scenarios to own folder

--- examples/teastore/scom_for_teastore.py
+++ examples/teastore/scom_for_teastore.py
@@ -4,7 +4,7 @@ import json
 
 files = ["auth.json", "image.json", "persistence.json", "recommender.json", "registry.json", "webui.json"]
 service_names = ["tools.descartes.teastore.auth", "tools.descartes.teastore.image", "tools.descartes.teastore.persistence", "tools.descartes.teastore.recommender", "tools.descartes.teastore.registry", "tools.descartes.teastore.webui"]
-path = "../grpc/"
+path = "../../grpc/"
 
 for i, f in enumerate(files): 
     file = open(f"{path}/{f}")
@@ -13,10 +13,10 @@ for i, f in enumerate(files):
 
     c = cohesion.calculate_scom(data, service_names[i], True)
     grouped_spans = span.get_grouped_spans_from_file(data, service_names[i])
-    #calls_per_table = span.get_number_of_calls_per_table_from_file(data, service_names[i])
+    calls_per_table = span.get_number_of_calls_per_table_from_file(data, service_names[i])
     endpoint_calls = span.get_number_of_endpoint_calls_from_file(data, service_names[i])
 
     print(f"Cohesion for Service {service_names[i]}: {c}")
     print(f"Grouped spans for {f}: {grouped_spans}")
-    #print(f"Number of calls per table: {calls_per_table}")
+    print(f"Number of calls per table: {calls_per_table}")
     print(f"Number of endpoint calls: {endpoint_calls}")
